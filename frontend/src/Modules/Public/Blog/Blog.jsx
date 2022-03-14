c749d808f93868112b9f4a3933312481d728f84d
DiepHoang19 <93962691+DiepHoang19@users.noreply.github.com>
2022-03-15 00:15:52 +0700

    update lamsondeptrai

--- frontend/src/Modules/Public/Blog/Blog.jsx
+++ frontend/src/Modules/Public/Blog/Blog.jsx
@@ -3,7 +3,9 @@ import React, { Component } from "react";
 export class Blog extends Component {
     constructor(props) {
         super(props);
-        this.state = {}
+        this.state = {
+            name:'lamsondepzai',
+        }
     }
     render() {
         return (
