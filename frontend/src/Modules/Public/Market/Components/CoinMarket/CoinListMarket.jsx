42a81f1f87cade926d5aafbed791372c187747c6
DiepHoang19 <93962691+DiepHoang19@users.noreply.github.com>
2022-03-22 12:17:11 +0700

    update

--- frontend/src/Modules/Public/Market/Components/CoinMarket/CoinListMarket.jsx
+++ frontend/src/Modules/Public/Market/Components/CoinMarket/CoinListMarket.jsx
@@ -93,6 +93,7 @@ export class CoinListMarket extends Component {
                     </thead>
                     <tbody>
                       {coinList.map((coin, index) => {
+                        
                         return (
                           <tr data-href="#" key={index} onClick={() => this.redirectToCoinDetail(coin)}>
                             <td>
