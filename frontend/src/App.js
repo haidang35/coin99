c40043654324b5d3c49ca7dcb358441022d97cff
lamson <sonlamkk011@gmail.com>
2022-03-23 14:40:47 +0700

    update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import { Admin } from "./Modules/Admin/Admin";
 import { Dashboard } from "./Modules/Admin/Dashboard/Components/Dashboard";
 import { Abouts } from "./Modules/Public/About/Abouts";
 import { Account } from "./Modules/Public/Account/Account";
+import { LogIn } from "./Modules/Public/Account/Components/LogIn";
 import { Contact } from "./Modules/Public/Contact/Contact";
 import { CoinList } from "./Modules/Public/Home/Components/CoinList/CoinList";
 import { Public } from "./Modules/Public/Public";
---
+++ frontend/src/Modules/Public/Account/Components/LogIn.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+
+export class LogIn extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+
+        }
+    }
+    render() {
+        return(
+            <>
+           
+            </>
+        )
+    }
+}
