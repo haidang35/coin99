1fa54c0ca7fc9bededdaa14861943a2f4f00cd4b
DiepHoang19 <93962691+DiepHoang19@users.noreply.github.com>
2022-03-24 10:48:59 +0700

    update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,9 @@
 import { Accounts, Logins } from "./Modules/Admin/Account/Accounts";
 import { Admin } from "./Modules/Admin/Admin";
 import { Dashboard } from "./Modules/Admin/Dashboard/Components/Dashboard";
-import { NewBlog } from "./Modules/Admin/Postt/Components/NewBlog";
 import { PostList } from "./Modules/Admin/Postt/Components/PostList";
 
-import { Post } from "./Modules/Admin/Postt/Post";
+
 import { Abouts } from "./Modules/Public/About/Abouts";
 import { Contact } from "./Modules/Public/Contact/Contact";
 import { CoinList } from "./Modules/Public/Home/Components/CoinList/CoinList";
