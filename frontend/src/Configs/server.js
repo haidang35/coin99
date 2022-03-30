be508d5a1936cbde28ac0f3736a5d3fc4878e34b
DiepHoang19 <93962691+DiepHoang19@users.noreply.github.com>
2022-03-30 14:51:07 +0700

    update

--- frontend/src/Configs/server.js
+++ frontend/src/Configs/server.js
@@ -1,4 +1,4 @@
 
-// export const BASE_URL_SERVER = 'https://coin99.azurewebsites.net';
-export const BASE_URL_SERVER = 'https://localhost:44340';
+export const BASE_URL_SERVER = 'https://coin99.azurewebsites.net';
+// export const BASE_URL_SERVER = 'https://localhost:44340';
 export const AUTH_TOKEN = localStorage.getItem('access_token');
