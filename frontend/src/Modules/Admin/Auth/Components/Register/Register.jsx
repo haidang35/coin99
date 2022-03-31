a9a7b2c19b1310f4f29b3c33207693ed36a0585d
DiepHoang19 <93962691+DiepHoang19@users.noreply.github.com>
2022-03-31 10:25:54 +0700

    update

--- frontend/src/Modules/Admin/Auth/Components/Register/Register.jsx
+++ frontend/src/Modules/Admin/Auth/Components/Register/Register.jsx
@@ -1,22 +1,36 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import { REGEX_TEL } from "../../../../../Configs/validation";
+import { ErrorForm } from "../../../../../Shared/Components/ErrorForm";
+import Form from "../../../../../Shared/Components/Form";
 import authService from "../../Services/AuthService";
 
 
-export class Register extends Component {
+
+export class Register extends Form {
   constructor(props) {
     super(props);
     this.state = {
-      form: {
-        FullName: '',
-        Birthday: '',
-        Email: '',
-        PhoneNumber: '',
-        Password: '',
-        ConfirmationPassword: ''
+      form: this._getInitFormData({
+        FullName: "",
+        Birthday: "",
+        Email: "",
+        PhoneNumber: "",
+        Password: "",
+        ConfirmationPassword: "",
+      }),
+      mesage: {
+        type: "",
+        content: "",
+
       }
+
     }
+
   }
+
+
+
 
   handleChangeSignup = (event) => {
     const { name, value } = event.target;
@@ -26,10 +40,24 @@ export class Register extends Component {
   }
 
   onSignup = async () => {
+    this._validateForm();
+    if (this._isFormValid()) {
+      const { FullName, Email, Birthday, PhoneNumber, Password, ConfirmationPassword } = this.state.form;
+      const data = {
+        FullName: FullName.value,
+        Birthday: Birthday.value,
+        Email: Email.value,
+        PhoneNumber: PhoneNumber.value,
+        Password: Password.value,
+        ConfirmationPassword: ConfirmationPassword.value,
+      }
+
+
+    }
     const { form } = this.state;
     await authService.registerNewUser(form)
       .then((res) => {
-         window.location.replace("/admin-login");
+        window.location.replace("/admin-login");
       })
       .catch((err) => {
         console.log(err);
@@ -46,7 +74,7 @@ export class Register extends Component {
                 <div className="login-wrapper crypto">
                   <div className="col-lg-6 col-sm-12 over-h hidden-sm no-padding-left  no-padding-right">
                     <img
-                      src= {window.location.origin + '/Assets/Admin/data/crypto-dash/login-img.png'}
+                      src={window.location.origin + '/Assets/Admin/data/crypto-dash/login-img.png'}
                       style={{ width: "105%" }}
                       alt=""
                     />
@@ -84,9 +112,21 @@ export class Register extends Component {
                                       className="form-control"
                                       name="FullName"
                                       placeholder="enter full name"
-                                      value={FullName}
-                                      onChange={this.handleChangeSignup}
+                                      required value={FullName.value} onChange={(ev) => this._setValue(ev, "Fullname")}
+
+
                                     />
+                                    {FullName.err !== "" ? (
+                                      FullName.err === "*" ? (
+                                        <ErrorForm
+                                          message={"Email cannot be empty"}
+                                        />
+                                      ) : (
+                                        <ErrorForm message={FullName.err} />
+                                      )
+                                    ) : (
+                                      ""
+                                    )}
                                   </div>
                                 </div>
                                 <div className="form-group">
@@ -97,22 +137,46 @@ export class Register extends Component {
                                       className="form-control"
                                       name="Birthday"
                                       placeholder="enter birthday"
-                                      value={Birthday}
-                                      onChange={this.handleChangeSignup}
+                                      required value={Birthday.value} onChange={(ev) => this._setValue(ev, "Birthday")}
                                     />
+                                    {Birthday.err !== "" ? (
+                                      Birthday.err === "*" ? (
+                                        <ErrorForm
+                                          message={"Birthday cannot be empty"}
+                                        />
+                                      ) : (
+                                        <ErrorForm message={Birthday.err} />
+                                      )
+                                    ) : (
+                                      ""
+                                    )}
                                   </div>
                                 </div>
                                 <div className="form-group">
                                   <label className="form-label">Email</label>
                                   <div className="controls">
                                     <input
-                                      type="text"
+                                      type="email"
                                       className="form-control"
                                       name="Email"
                                       placeholder="enter email"
-                                      value={Email}
-                                      onChange={this.handleChangeSignup}
+                                      required value={Email.value} onChange={(ev) => this._setValue(ev, "Email")}
                                     />
+                                    {
+                                      Email.err !== '' ? Email.err === "*" ? <ErrorForm message="Email cannot be empty" /> : <ErrorForm message={Email.err} /> : ""
+                                    }
+                                    {Email.err !== "" ? (
+                                      Email.err === "*" ? (
+                                        <ErrorForm
+                                         
+                                        />
+                                      ) : (
+                                        <ErrorForm message={Email.err} />
+                                      )
+                                    ) : (
+                                      ""
+                                    )}
+
                                   </div>
                                 </div>
                                 <div className="form-group">
@@ -123,9 +187,21 @@ export class Register extends Component {
                                       className="form-control"
                                       name="PhoneNumber"
                                       placeholder="enter phone number"
-                                      value={PhoneNumber}
-                                      onChange={this.handleChangeSignup}
+                                      required value={PhoneNumber.value} onChange={(ev) => this._setValue(ev, "PhoneNumber")}
+                                      pattern={REGEX_TEL}
                                     />
+                                    {PhoneNumber.err !== "" ? (
+                                      PhoneNumber.err === "*" ? (
+                                        <ErrorForm
+                                          message={"Phone Number cannot be empty"}
+                                        />
+                                      ) : (
+                                        <ErrorForm message={PhoneNumber.err} />
+                                      )
+                                    ) : (
+                                      ""
+                                    )}
+
                                   </div>
                                 </div>
                                 <div className="form-group">
@@ -136,9 +212,19 @@ export class Register extends Component {
                                       className="form-control"
                                       name="Password"
                                       placeholder="enter password"
-                                      value={Password}
-                                      onChange={this.handleChangeSignup}
+                                      required value={Password.value} onChange={(ev) => this._setValue(ev, "Password")}
                                     />
+                                    {Password.err !== "" ? (
+                                      Password.err === "*" ? (
+                                        <ErrorForm
+                                          message={"Password cannot be empty"}
+                                        />
+                                      ) : (
+                                        <ErrorForm message={Password.err} />
+                                      )
+                                    ) : (
+                                      ""
+                                    )}
                                   </div>
                                 </div>
                                 <div className="form-group">
@@ -151,9 +237,19 @@ export class Register extends Component {
                                       className="form-control"
                                       name="ConfirmationPassword"
                                       placeholder="repeat password"
-                                      value={ConfirmationPassword}
-                                      onChange={this.handleChangeSignup}
+                                      required value={ConfirmationPassword.value} onChange={(ev) => this._setValue(ev, "ConfirmationPassword")}
                                     />
+                                    {ConfirmationPassword.err !== "" ? (
+                                      ConfirmationPassword.err === "*" ? (
+                                        <ErrorForm
+                                          message={"ConfirmationPassword cannot be empty"}
+                                        />
+                                      ) : (
+                                        <ErrorForm message={ConfirmationPassword.err} />
+                                      )
+                                    ) : (
+                                      ""
+                                    )}
                                   </div>
                                 </div>
                                 <div className="text-center">
